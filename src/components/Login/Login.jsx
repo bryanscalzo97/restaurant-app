ae63c19750eb604357f6afad0b941376bc82fcbe
Bryan Scalzo <50776347+bryanscalzo97@users.noreply.github.com>
2022-03-17 14:57:56 -0300

    adding login layout

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,59 @@
+import {
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Stack,
+  Image,
+} from "@chakra-ui/react";
+
+export default function Login() {
+  return (
+    <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={4} w={"full"} maxW={"md"}>
+          <h1>Welcome to Scalzo Restaurant</h1>
+          <Heading fontSize={"2xl"}>Sign in to your account</Heading>
+          <FormControl id="email">
+            <FormLabel>Email address</FormLabel>
+            <Input type="email" />
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel>Password</FormLabel>
+            <Input type="password" />
+          </FormControl>
+          <Stack spacing={6}>
+            <Stack
+              direction={{ base: "column", sm: "row" }}
+              align={"start"}
+              justify={"space-between"}
+            >
+              <Checkbox>Remember me</Checkbox>
+              <Link color={"blue.500"}>Forgot password?</Link>
+            </Stack>
+            <Button
+              colorScheme={"blue"}
+              variant={"solid"}
+              onClick={() => alert("que emocion")}
+            >
+              Sign in
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={"Login Image"}
+          objectFit={"cover"}
+          src={
+            "https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+          }
+        />
+      </Flex>
+    </Stack>
+  );
+}
